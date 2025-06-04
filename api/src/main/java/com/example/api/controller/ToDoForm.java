1d3ff84ddf510b53357bb228572b1125cdc13954
shunsuke1909 <shunsuke.sakaya1901@gmail.com>
2025-06-04 15:32:55 +0900

    Use is_completed mapping

--- api/src/main/java/com/example/api/controller/ToDoForm.java
+++ api/src/main/java/com/example/api/controller/ToDoForm.java
@@ -1,9 +1,11 @@
 package com.example.api.controller;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 //フォームで送られてきた入力内容を、一時的にまとめて受け取るための入れ物（データの箱）
 public class ToDoForm {
     public String title;
+
+    @JsonProperty("is_completed")
     public Boolean isCompleted;
 }
